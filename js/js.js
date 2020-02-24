96e0b616dc2070c2630655050dc4336904495367
David <eaadpa@students.eaaa.dk>
2020-02-24 10:18:04 +0100

    1

---
+++ js/js.js
@@ -0,0 +1,20 @@
+
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+var i=4;
+var j=4;
+
+document.write("csaa");
+
+inner.html("asd");
