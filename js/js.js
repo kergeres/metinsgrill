141416bc1d6d583051ce59243e875a29a7cac927
David <eaadpa@students.eaaa.dk>
2020-02-25 13:47:06 +0100

    beautiful

--- js/js.js
+++ js/js.js
@@ -1,20 +1,2 @@
 
 
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
-
-var i=4;
-var j=4;
-
-document.write("csaa");
-
-inner.html("asd");
